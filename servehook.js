5786638854ea67901f1e44b7060616ea9e94633e
kristenmills <kristen@kristen-mills.com>
2015-04-09 19:15:41 -0400

    uses express now

---
+++ servehook.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+function checkLocation (loc) {
+  // 43.083849, -77.679825
+  var center = {
+    x : 43.083849,
+    y : -77.679825
+  };
+  var radius = 0.001;
+  return Math.pow(loc[0] - center.x, 2) + Math.pow(loc[1] - center.y, 2) < Math.pow(radius, 2);
+}
+
+app.get('/', function(req, res, next) {
+  if(req.query.location){
+    var user = req.query.username;
+    var location = req.query.location.split(';');
+    if (checkLocation(location)) {
+      console.log("Wave Pls");
+      console.log(user, location);
+    } else {
+      console.log("Too Far");
+    }
+  }
+
+  res.send();
+});
+
+app.set('port', process.env.PORT || 8000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
